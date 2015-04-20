f2ac8bdb58d914abedcd35e9a28593c6f4b9cf30
lynnmedia <github@winmedia.com>
2015-04-20 15:01:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,10 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var mongoose = require('mongoose');
-var	users = {};
-	
 server.listen(8000);
 
-mongoose.connect('mongodb://ChatStorage:eWc7tyQHN9rfbazEF9RhjSsHsmHy02Cu_QtRtD.BY.E-@ds027758.mongolab.com:27758/ChatStorage', function (err) {
-	if(err){
-		console.log(err);
-	} else{
-		console.log('Connected to mongodb!');
-	}
-});
-
-var chatSchema = mongoose.Schema({
-	user: String,
-	msg: String,
-	created: {type: Date, default: Date.now}
-});
-
-var Chat = mongoose.model('Message', chatSchema);
-
 io.sockets.on('connection', function(socket){
-	var query = Chat.find({});
-	query.sort('-created').limit(8).exec(function(err, docs){
-		if(err) throw err;
-		socket.emit('load old msgs', docs);
-	});
-	
-	socket.on('new user', function(data, callback){
-		if (data in users){
-			callback(false);
-		} else{
-			callback(true);
-			socket.username = data;
-			users[socket.username] = socket;
-			updateUsers();
-		}
-	});
-	
-	function updateUsers(){
-		io.sockets.emit('usernames', Object.keys(users));
-	}
-
-	socket.on('send message', function(data, callback){
-		var msg = data.trim();
-		console.log('after trimming message is: ' + msg);
-		if(msg.substr(0,3) === '/w '){
-			msg = msg.substr(3);
-			var ind = msg.indexOf(' ');
-			if(ind !== -1){
-				var name = msg.substring(0, ind);
-				var msg = msg.substring(ind + 1);
-				if(name in users){
-					users[name].emit('whisper', {msg: msg, user: socket.username});
-					console.log('message sent is: ' + msg);
-					console.log('Whisper!');
-				} else{
-					callback('Error!  Enter a valid user.');
-				}
-			} else{
-				callback('Error!  Please enter a message for your whisper.');
-			}
-		} else{
-			var newMsg = new Chat({msg: msg, user: socket.username});
-			newMsg.save(function(err){
-				if(err) throw err;
-				io.sockets.emit('new message', {msg: msg, user: socket.username});
-			});
-		}
-	});
-	
-	socket.on('disconnect', function(data){
-		if(!socket.username) return;
-		delete users[socket.username];
-		updateUsers();
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', data);
 	});
 });
